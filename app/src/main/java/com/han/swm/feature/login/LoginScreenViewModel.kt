28042ce088681e8d2c522248ef4f633432505488
Bich-Huyen <21522174@gm.uit.edu.vn>
2024-12-25 14:03:20 +0700

    Create Login screen logic

---
+++ app/src/main/java/com/han/swm/feature/login/LoginScreenViewModel.kt
@@ -0,0 +1,10 @@
+package com.han.swm.feature.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginScreenViewModel: ViewModel() {
+
+    fun onLogin(email: String, password: String) {
+
+    }
+}
