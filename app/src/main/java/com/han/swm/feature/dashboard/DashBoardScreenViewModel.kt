f720b997a27ff2a5d869d3bd5d60af927c532cd6
vuhoang-gr <21520243@gm.uit.edu.vn>
2024-12-28 00:36:52 +0100

    Implement dashboard logic

--- app/src/main/java/com/han/swm/feature/dashboard/DashBoardScreenViewModel.kt
+++ app/src/main/java/com/han/swm/feature/dashboard/DashBoardScreenViewModel.kt
@@ -1,16 +1,56 @@
 package com.han.swm.feature.dashboard
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.han.swm.domain.model.statisticMock
 import com.han.swm.domain.model.userMock
+import com.han.swm.domain.model.weekStatisticMock
+import com.han.swm.singleton.RepositorySingleton
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class DashBoardScreenViewModel: ViewModel() {
-    private val _uiState = MutableStateFlow<DashBoardScreenUiState>(DashBoardScreenUiState(user = userMock))
+    private val authRepository = RepositorySingleton.authRepository
+    private val statisticsRepository = RepositorySingleton.statisticsRepository
+
+    private val _uiState = MutableStateFlow<DashBoardScreenUiState>(DashBoardScreenUiState(user = userMock, weekStatistic = weekStatisticMock, statistic = statisticMock))
     val uiState: StateFlow<DashBoardScreenUiState> = _uiState.asStateFlow()
 
-    fun onChangeDashBoardType(type: DashBoardType) {
+    init {
+        viewModelScope.launch {
+            try {
+                val userGet = async { authRepository.getProfile() }
+                val weekStatisticGet = async { statisticsRepository.getWeekStatistic() }
+                val statisticGet = async { statisticsRepository.getStatistic() }
 
+                val user = userGet.await()
+                val weekStatistic = weekStatisticGet.await()
+                val statistic = statisticGet.await()
+
+                _uiState.update {
+                    _uiState.value.copy(
+                        user = user,
+                        weekStatistic = weekStatistic,
+                        statistic = statistic
+                    )
+                }
+            } catch (e: Exception) {
+                Log.d("HanDebug", "bug: ${e.message}")
+            }
+        }
+    }
+
+    fun onChangeDashBoardType(type: DashBoardType) {
+        _uiState.update {
+            _uiState.value.copy(
+                dashBoardType = type
+            )
+        }
     }
 }
